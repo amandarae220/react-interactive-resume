1edb1962fc61a1068450d1c26b9f37a16b3bc42e
AL <57809794+amandarae220@users.noreply.github.com>
2025-03-02 10:42:07 -0500

    prepping for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/react-interactive-resume/",
+  base: '/react-interactive-resume/', // ensure this matches your repo name
   plugins: [react()],
 });
